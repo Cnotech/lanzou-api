14e74a89d0b2e6fa536bf6fe81dc5237bab6b1ae
Cno <dongheming@bytedance.com>
2024-10-13 01:11:50 +0800

    feat: 支持请求缓存

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,30 @@
 import { Err, Ok, Result } from "ts-results-es";
-import { CheerioAPI, load } from "cheerio";
+import { CheerioAPI } from "cheerio";
 import { ShareType } from "./types";
+import { existsSync } from "node:fs";
+import { CACHE_DIR } from "./constants";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
+import { join } from "node:path";
+
+function calcMD5(text: string) {
+  return createHash("md5").update(text).digest("hex");
+}
 
 export async function fetchPage(url: string): Promise<Result<string, string>> {
+  if (!existsSync(CACHE_DIR)) {
+    await mkdir(CACHE_DIR);
+  }
+  const md5 = calcMD5(url);
+  const cachePath = join(CACHE_DIR, md5);
+  if (existsSync(cachePath)) {
+    const h = await readFile(cachePath);
+    return new Ok(h.toString());
+  }
   try {
     const res = await fetch(url);
     const t = await res.text();
+    await writeFile(cachePath, t);
     return new Ok(t);
   } catch (e) {
     return new Err(
